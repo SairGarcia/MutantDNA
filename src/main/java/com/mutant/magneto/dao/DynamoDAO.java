60c320efa36b4e6eae471c51c844cafe009fd12d
SairGarcia <50638768+SairGarcia@users.noreply.github.com>
2021-02-24 15:43:53 -0500

    Update DynamoDAO.java

--- src/main/java/com/mutant/magneto/dao/DynamoDAO.java
+++ src/main/java/com/mutant/magneto/dao/DynamoDAO.java
@@ -1,6 +1,7 @@
 package com.mutant.magneto.dao;
 
-import com.amazonaws.regions.Regions;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
@@ -12,7 +13,14 @@ public abstract class DynamoDAO {
 	protected DynamoDBMapper mapper;
 	
 	public DynamoDAO() {
-	    client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_2).build();
+	    //client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_2).build();
+	    String serviceEndpoint = "dynamodb.us-east-2.amazonaws.com";
+	    String signingRegion = "us-east-2";
+	    String accessKey = "AKIAZARQOEQ5HANEKZWT";
+	    String secretKey = "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";
+	    client = AmazonDynamoDBClientBuilder.standard()
+			.withEndpointConfiguration(new AmazonDynamoDBClientBuilder.EndpointConfiguration(serviceEndpoint, signingRegion))
+			.withCredentials (new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey))).build();
 	    mapper = new DynamoDBMapper(client);
 	}
 }
